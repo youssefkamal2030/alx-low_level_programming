690eb9bf7b5c03028c8a5ddbcc787e2982ae2e77
youssef <youssefkamal856@yahoo.com>
2023-09-11 23:11:11 +0300

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+if ((char) i == 'p' || (char) i == 'q')
+continue;
+else
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
