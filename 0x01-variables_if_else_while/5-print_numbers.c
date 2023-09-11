0c20d83cea5f46cfe26c9e77742bd8c6e58189a3
youssef <youssefkamal856@yahoo.com>
2023-09-11 23:26:03 +0300

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - variables_if_else_while
+ *
+ * Description: '5-print_numbers.c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
