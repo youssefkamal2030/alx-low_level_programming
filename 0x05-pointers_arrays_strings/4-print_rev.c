f8ada72a9b5efc43b3adf5169d83ca232075b669
youssef <youssefkamal856@yahoo.com>
2023-09-18 13:39:06 +0300

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
  *  * print_rev - prints a string in reverse, followed by a new line
  *   * @s: string to be printed
  */
-void _puts(char *str)
+void print_rev(char *str)
 {
 	int i = strlen(str);
 
