fcde8005c2c112fc04ce444bf6068a6b371549a5
youssef <youssefkamal856@yahoo.com>
2023-09-20 18:11:08 +0300

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define HOLBERTON_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
