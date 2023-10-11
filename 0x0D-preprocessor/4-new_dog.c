b62714c4e6b46dcbaca423aa8a0923fe5dd86f29
youssef <youssefkamal856@yahoo.com>
2023-10-11 16:53:04 +0300

    done

---
+++ 0x0D-preprocessor/4-new_dog.c
@@ -0,0 +1,10 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ *  * init_dog - initializes a variable of type struct dog
+ *   * @d: pointer to struct dog to initialize
+ *    * @name: name to initialize
+ *     * @age: age to initialize
+ *      * @owner: owner to initialize
+ */
+void init_do:g(struct 
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,4 @@
+#ifndef main.h
+#define main.h
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
