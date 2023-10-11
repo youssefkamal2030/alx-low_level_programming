645e3f0f0fecb4197a38c76953b6aaee7f6cb69c
youssef <youssefkamal856@yahoo.com>
2023-10-11 16:56:12 +0300

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog.h
+#define doh.h
+/**
+ *  * struct dog - a dog's basic info
+ *   * @name: First member
+ *    * @age: Second member
+ *     * @owner: Third member
+ *      *
+ *       * Description: Longer description
+ */
+struct dog{
+	char *name;
+	float age;
+	char* owner;
+};
+#endif
