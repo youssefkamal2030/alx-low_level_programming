a6bd6599a99f3c827fa3570d3a6358fda52cf9dc
youssef <youssefkamal856@yahoo.com>
2023-11-01 21:39:46 +0200

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,6 @@
 
 #define READ_BUF_SIZE 1024
 
-int _putchar(char c);
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
