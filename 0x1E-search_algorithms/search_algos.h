8c1f73bcc52590c4addc1fe2b22aebcfd7cb7117
youssef <youssefkamal856@yahoo.com>
2024-07-17 21:11:04 +0300

    done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,29 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-int linear_search(int *array, size_t size, int value);
+ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 
-#endif /* SEARCH_ALGOS_H */
+/* LIBRARIES */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/* STRUCTURES */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+        int n;
+        size_t index;
+        struct listint_s *next;
+} listint_t;
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
